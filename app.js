0769f6b4bfade55bb7c510a83c6d58fb4e180492
gonzalo10 <gonzalo.achiaga@gmail.com>
2019-03-22 20:46:36 +0100

    problem fixed, it was on app.js

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-const http = require('http');
-const path = require('path')
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars')
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('handlebars', expressHbs());
-app.set('view engine', 'handlebars');
+app.engine(
+  'hbs',
+  expressHbs({
+    layoutsDir: 'views/layouts/',
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+  })
+);
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle:'Page not found!!'}); // mira automáticamente en el view folder porque se lo hemos dicha arriba en el app.set
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
-
 
 app.listen(3000);
