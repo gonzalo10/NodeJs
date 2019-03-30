23ae257e6c2da76bc7bf24d9cec135e1c3de567c
gonzalo10 <gonzalo.achiaga@gmail.com>
2019-03-30 21:36:41 +0100

    sequelize integration completed

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,10 +2,10 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows]) => {
+  Product.findAll()
+    .then(products => {
       res.render('shop/product-list', {
-        prods: rows,
+        prods: products,
         pageTitle: 'All Products',
         path: '/products'
       });
@@ -15,10 +15,19 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
+  // Product.findAll({where: {id: prodId}})
+  // .then(products => {
+  //   res.render('shop/product-detail', {
+  //     product: products[0],
+  //     pageTitle: products[0].title,
+  //     path: '/products'
+  //   });
+  // })
+  // .catch(err => console.log(err))
+  Product.findByPk(prodId)
+    .then(product => {
       res.render('shop/product-detail', {
-        product: product[0],
+        product: product,
         pageTitle: product.title,
         path: '/products'
       });
@@ -27,15 +36,16 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows]) => {
-      res.render('shop/index', {
-        prods: rows,
-        pageTitle: 'Shop',
-        path: '/'
-      });
-    })
-    .catch(err => console.log(err));
+  Product
+  .findAll()
+  .then(product => {
+    res.render('shop/index', {
+      prods: product,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  })
+  .catch(err => console.log(err));
 };
 
 exports.getCart = (req, res, next) => {
